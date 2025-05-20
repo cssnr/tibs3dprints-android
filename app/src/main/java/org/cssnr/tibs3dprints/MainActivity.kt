4d535aec3d148e30132afd5b842a41e306484f63
Shane <6071159+smashedr@users.noreply.github.com>
2025-05-19 21:18:29 -0700

    Update Theme to Material3

--- app/src/main/java/org/cssnr/tibs3dprints/MainActivity.kt
+++ app/src/main/java/org/cssnr/tibs3dprints/MainActivity.kt
@@ -3,6 +3,7 @@ package org.cssnr.tibs3dprints
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -26,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
