23b95db21922168edbd22a01788df0cfccc8649b
Shane <6071159+smashedr@users.noreply.github.com>
2025-06-10 14:20:35 -0700

    Update launchNotificationSettings

--- app/src/main/java/org/cssnr/tibs3dprints/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/cssnr/tibs3dprints/ui/settings/SettingsFragment.kt
@@ -308,42 +308,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 }
 
-fun Context.requestPerms(
-    requestPermissionLauncher: ActivityResultLauncher<String>,
-    newValue: Boolean,
-) {
-    if (newValue == false) {
-        launchNotificationSettings()
-        return
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        val perm = Manifest.permission.POST_NOTIFICATIONS
-        when {
-            ContextCompat.checkSelfPermission(this, perm) ==
-                    PackageManager.PERMISSION_GRANTED -> {
-                Log.d("requestPerms", "1 - Permission Already Granted")
-                launchNotificationSettings()
-            }
-
-            ActivityCompat.shouldShowRequestPermissionRationale(this as Activity, perm) -> {
-                Log.d("requestPerms", "2 - Permissions Denied, Show Main Alert Section")
-                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
-                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-                }
-                startActivity(intent)
-            }
-
-            else -> {
-                Log.d("requestPerms", "3 - Else: requestPermissionLauncher")
-                requestPermissionLauncher.launch(perm)
-            }
-        }
-    } else {
-        Log.i("requestPerms", "4 - PRE API 33, User Managed Only")
-        launchNotificationSettings()
-    }
-}
-
 fun Context.sendNotification(builder: NotificationCompat.Builder) {
     val intent = Intent(this, MainActivity::class.java).apply {
         action = "org.cssnr.tibs3dprints.ACTION_NOTIFICATION"
@@ -368,6 +332,55 @@ fun Context.sendNotification(builder: NotificationCompat.Builder) {
     }
 }
 
+fun Context.launchNotificationSettings(channelId: String = "default_channel_id") {
+    val notificationManager = NotificationManagerCompat.from(this)
+    val globalEnabled = notificationManager.areNotificationsEnabled()
+    Log.i("areNotificationsEnabled", "globalEnabled: $globalEnabled")
+    val intent = if (globalEnabled) {
+        Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
+            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            putExtra(Settings.EXTRA_CHANNEL_ID, channelId)
+        }
+    } else {
+        Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        }
+    }
+    startActivity(intent)
+}
+
+fun Context.requestPerms(
+    requestPermissionLauncher: ActivityResultLauncher<String>,
+    newValue: Boolean,
+) {
+    if (newValue == false) {
+        launchNotificationSettings()
+        return
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val perm = Manifest.permission.POST_NOTIFICATIONS
+        when {
+            ContextCompat.checkSelfPermission(this, perm) ==
+                    PackageManager.PERMISSION_GRANTED -> {
+                Log.d("requestPerms", "1 - Permission Already Granted")
+                launchNotificationSettings()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(this as Activity, perm) -> {
+                launchNotificationSettings()
+            }
+
+            else -> {
+                Log.d("requestPerms", "3 - Else: requestPermissionLauncher")
+                requestPermissionLauncher.launch(perm)
+            }
+        }
+    } else {
+        Log.i("requestPerms", "4 - PRE API 33, User Managed Only")
+        launchNotificationSettings()
+    }
+}
+
 fun Context.areNotificationsEnabled(): Boolean {
     val notificationManager = NotificationManagerCompat.from(this)
     return when {
@@ -378,12 +391,4 @@ fun Context.areNotificationsEnabled(): Boolean {
             } == null
         }
     }
-}
-
-fun Context.launchNotificationSettings() {
-    val intent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
-        putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-        putExtra(Settings.EXTRA_CHANNEL_ID, "default_channel_id")
-    }
-    startActivity(intent)
 }
